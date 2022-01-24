1cd3e1d6e5e1ee30a6f7400251ff4c2275b00c23
Phil Jirsa <phil.jirsa@microsoft.com>
2022-01-23 18:28:07 -0600

    bump app version

--- hackathonTeamBuilder/tabs/src/index.jsx
+++ hackathonTeamBuilder/tabs/src/index.jsx
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
+console.log(process.env.NODE_ENV);
 ReactDOM.render(<App />, document.getElementById("root"));
