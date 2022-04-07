35fadbed224e5b152b93b64834c78558020cdb46
mjschanne <mjschanne@microsoft.com>
2022-04-07 10:15:37 -0400

    Add TeamBuilder.Serverless

---
+++ TeamBuilder.Serverless/AddGitHubRepository.cs
@@ -0,0 +1,18 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+
+namespace TeamBuilder.Serverless
+{
+    public static class AddGitHubRepository
+    {
+        [FunctionName("AddGitHubRepository")]
+        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        {
+            log.LogInformation(eventGridEvent.Data.ToString());
+        }
+    }
+}
---
+++ TeamBuilder.Serverless/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using TeamBuilder.Serverless;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace TeamBuilder.Serverless;
+
+public class Startup : FunctionsStartup
+{
+
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+    {
+        FunctionsHostBuilderContext context = builder.GetContext();
+
+        builder.ConfigurationBuilder
+            .SetBasePath(context.ApplicationRootPath)
+            .AddEnvironmentVariables();
+    }
+
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+
+    }
+}
