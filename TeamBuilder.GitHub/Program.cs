16ad8a0b9ebbfb2df8ee913ac62137f2da7b20f1
mjschanne <mjschanne@microsoft.com>
2022-04-07 10:12:21 -0400

    Always show swagger UI for GitHubAPI

--- TeamBuilder.GitHub/Program.cs
+++ TeamBuilder.GitHub/Program.cs
@@ -25,12 +25,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
