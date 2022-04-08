0f3fb9035661f8776ae26f7ef7a3ff6fd4c2f7ce
mjschanne <mjschanne@microsoft.com>
2022-04-08 12:27:12 -0400

    Az fx in prog

--- TeamBuilder.Serverless/Functions/AddGitHubRepository.cs
+++ TeamBuilder.Serverless/Functions/AddGitHubRepository.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
+using TeamBuilder.Models;
 
 namespace TeamBuilder.Serverless.Functions;
 
@@ -12,8 +13,16 @@ public static class AddGitHubRepository
     [FunctionName("AddGitHubRepository")]
     public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
     {
-        log.LogInformation(eventGridEvent.Data.ToString());
+        //log.LogInformation(eventGridEvent.Data.ToString());
 
-        //eventGridEvent.Data as Team
+        //var appSetting = eventGridEvent.Data as AppSetting;
+
+        //// only interested in changes to this appsetting where value is not null
+        //if (appSetting == null || appSetting?.Setting != "GIT_HUB_ORG" || string.IsNullOrEmpty(appSetting?.Value))
+        //    return; 
+
+        // call github api to create repository and add to the org team
     }
 }
+
+//df500a57
