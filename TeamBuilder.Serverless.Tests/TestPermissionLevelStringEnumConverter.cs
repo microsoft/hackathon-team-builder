a6ef63766df530dfbddea29ee9eb510050045c29
Phil Jirsa <phil.jirsa@microsoft.com>
2022-04-22 11:22:03 -0500

    add unit test for custom jsonconverter

---
+++ TeamBuilder.Serverless.Tests/TestPermissionLevelStringEnumConverter.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamBuilder.Serverless.Services;
+
+namespace TeamBuilder.Serverless.Tests
+{
+    public class TestPermissionLevelStringEnumConverter
+    {
+        private PermissionLevelStingEnumJsonConverter _permissionLevelConverter;
+        private string _sampleRequestJson;
+
+        [SetUp]
+        public void Setup()
+        {
+            _permissionLevelConverter = new PermissionLevelStingEnumJsonConverter();
+            _sampleRequestJson = File.ReadAllText("TestData/githubteampayload.json");
+        }
+
+        [Test]
+        public void TestCustomConverterShouldParsePermissionLevel()
+        {
+            Team req = JsonConvert.DeserializeObject<Team>(_sampleRequestJson, _permissionLevelConverter);
+
+            Assert.AreEqual("pull", req.Permission.StringValue);
+        }
+    }
+}
